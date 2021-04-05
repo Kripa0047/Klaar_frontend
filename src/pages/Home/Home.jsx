ed898ba3894fdebfbbeae817e4a96a2e5364ba33
kripa0047 <kripabairagi0047@gmail.com>
2021-04-05 10:25:37 +0530

    initial commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import { React } from 'react';
+import { Layout } from 'antd';
+import './Home.less';
+
+const Home = () => {
+    return (
+        <Layout>
+            <Layout.Header className="header">
+                WELCOME
+            </Layout.Header>
+        </Layout>
+    );
+}
+
+export default Home;
